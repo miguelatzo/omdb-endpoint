e9073ab5185725b4e0acb68a7e8305736a7f8d1a
Gerson Flores <produccion@mitoti.mx>
2024-10-26 13:47:36 -0600

    remove hard-coded test code
    Signed-off-by: Gerson Flores <produccion@mitoti.mx>

--- index.py
+++ index.py
@@ -20,8 +20,6 @@ def movie_search(hint):
     #urlunsplit: scheme, location, path, query, "" 
     url = urlunsplit((dotenv_values(".env")['OMDB_API_SCHEME'], dotenv_values(".env")['OMDB_API_LOC'], "", query_parameters, ""))
 
-    single_hard_coded_search_result = {"Title":"The Fast and the Furious","Year":"2001","Rated":"PG-13","Released":"22 Jun 2001","Runtime":"106 min","Genre":"Action, Crime, Thriller","Director":"Rob Cohen","Writer":"Ken Li, Gary Scott Thompson, Erik Bergquist","Actors":"Vin Diesel, Paul Walker, Michelle Rodriguez","Plot":"Los Angeles police officer Brian O'Conner must decide where his loyalty really lies when he becomes enamored with the street racing world he has been sent undercover to end it.","Language":"English, Spanish","Country":"United States, Germany","Awards":"11 wins & 18 nominations","Poster":"https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"6.8/10"},{"Source":"Rotten Tomatoes","Value":"54%"},{"Source":"Metacritic","Value":"58/100"}],"Metascore":"58","imdbRating":"6.8","imdbVotes":"426,570","imdbID":"tt0232500","Type":"movie","DVD":"N/A","BoxOffice":"$144,745,925","Production":"N/A","Website":"N/A","Response":"True"}
-
     try:
         movie_search = requests.get(url).json()
         logger.info(f"Search: {hint}")
